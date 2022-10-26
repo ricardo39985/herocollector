11a123f4200e71042f04f32ed3c13876b07b00b9
ricardo39985 <ricardo39985@gmail.com>
2022-10-26 18:45:49 -0400

    Add hero, photo Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Hero(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  user= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
