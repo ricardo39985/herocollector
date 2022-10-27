e4746632312927c453a38361c47c1174c3bc6bcb
ricardo39985 <ricardo39985@gmail.com>
2022-10-26 21:20:58 -0400

    Add date fields

--- main/models.py
+++ main/models.py
@@ -7,7 +7,12 @@ class Hero(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   user= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
 class Photo(models.Model):
   url = models.CharField(max_length=200)
   hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
